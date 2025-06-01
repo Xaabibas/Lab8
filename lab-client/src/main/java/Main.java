910facfd4725e61a5e2dbd76248c990d8e1cd0e6
Xaabibas <tumuyhabirov@gmail.com>
2025-06-01 13:34:39 +0300

    narisoval startovoe okno, tebe ne ponravitsa noviy class

--- lab-client/src/main/java/Main.java
+++ lab-client/src/main/java/Main.java
@@ -2,24 +2,37 @@ import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.Scanner;
 
+import ProcessEngine.GraphicCore.SignWindow.StartWindow;
 import ProcessEngine.ProcessRun;
 import ProcessEngine.ProcessCore.networkModule.NetworkManager;
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-public class Main {
+public class Main extends Application {
 
     protected static final int port = 46789;
     protected static final String host = "localhost";
 
     public static void main(String[] args) {
-
-        Scanner scanner = new Scanner(System.in);
         try {
+            Scanner scanner = new Scanner(System.in);
             NetworkManager networkManager = new NetworkManager(port, InetAddress.getByName(host));
             ProcessRun runner = new ProcessRun(scanner, networkManager);
-
-            runner.interactiveMode();
+            new Thread(runner::interactiveMode).start();
         } catch (UnknownHostException e) {
             System.out.println("[ERROR] Неизвестный хост");
+            System.exit(1);
         }
+        Application.launch();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        new StartWindow().registerWindow(stage);
+    }
+
+    @Override
+    public void stop() throws Exception {
+        System.exit(0);
     }
 }
